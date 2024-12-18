acfbd37a8fce75fe36b965b309eef9a74953aadf
Foued <foued.attar@lyceeastier.com>
2024-12-18 10:53:04 +0100

    caca

---
+++ src/Coureur.java
@@ -0,0 +1,5 @@
+public class Coureur extends Personne {
+    Categorie categorie;
+    int id;
+}
+
---
+++ src/GestionDesCoureurs.java
@@ -0,0 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GestionDesCoureurs {
+}
